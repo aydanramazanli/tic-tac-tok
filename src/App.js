a88d63338648723a1ce297a44277b91cc7a6bde6
Aydan <aydan2903@gmail.com>
2022-04-18 12:58:32 +0400

    tic tac tok game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './components/main';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+        <Main/>
+    </div>
+  );
+}
+
+export default App;
